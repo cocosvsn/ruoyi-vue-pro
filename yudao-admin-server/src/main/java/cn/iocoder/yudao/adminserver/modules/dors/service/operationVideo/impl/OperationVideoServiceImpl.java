9e7ec943f3bca2f97d2cfdf219f9679d739fe2c4
鲜de芒果 <bo_huang@shssyw.com>
2021-12-04 15:37:39 +0800

    手术视频删除时同时删除文件。

--- yudao-admin-server/src/main/java/cn/iocoder/yudao/adminserver/modules/dors/service/operationVideo/impl/OperationVideoServiceImpl.java
+++ yudao-admin-server/src/main/java/cn/iocoder/yudao/adminserver/modules/dors/service/operationVideo/impl/OperationVideoServiceImpl.java
@@ -3,10 +3,14 @@ package cn.iocoder.yudao.adminserver.modules.dors.service.operationVideo.impl;
 import cn.iocoder.yudao.adminserver.modules.dors.dal.dataobject.videoFile.VideoFileDO;
 import cn.iocoder.yudao.adminserver.modules.dors.dal.mysql.videoFile.VideoFileMapper;
 import cn.iocoder.yudao.framework.file.config.FileProperties;
+import org.apache.commons.lang3.StringUtils;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
 import javax.annotation.Resource;
 import org.springframework.validation.annotation.Validated;
 
+import java.io.File;
 import java.util.*;
 import cn.iocoder.yudao.adminserver.modules.dors.controller.operationVideo.vo.*;
 import cn.iocoder.yudao.adminserver.modules.dors.dal.dataobject.operationVideo.OperationVideoDO;
@@ -28,6 +32,9 @@ import static cn.iocoder.yudao.adminserver.modules.dors.enums.DorsErrorCodeConst
 @Validated
 public class OperationVideoServiceImpl implements OperationVideoService {
 
+    private Logger logger = LoggerFactory.getLogger(getClass());
+    @Resource
+    private FileProperties fileProperties;
     @Resource
     private VideoFileMapper videoFileMapper;
     @Resource
@@ -57,6 +64,23 @@ public class OperationVideoServiceImpl implements OperationVideoService {
         this.validateOperationVideoExists(id);
         // 删除
         operationVideoMapper.deleteById(id);
+        // 把相应通道的文件也删除，并回收磁盘空间。
+        List<VideoFileDO> videoFileDOS = this.videoFileMapper.selectList("operation_video", id);
+        for (VideoFileDO vf: videoFileDOS) {
+            // 删除视频文件
+            File channelVideoFile = new File(fileProperties.getLocal().getDirectory()+vf.getRelativePath());
+            if (channelVideoFile.exists()) {
+                logger.info("delete video file【{}】: {}", channelVideoFile.getAbsolutePath(), channelVideoFile.delete());
+            }
+            // 删除海报文件
+            File channelPosterFile = new File(
+                    StringUtils.substringBeforeLast(channelVideoFile.getAbsolutePath(), ".")+".jpg");
+            if (channelPosterFile.exists()) {
+                logger.info("delete poster file【{}】: {}", channelPosterFile.getAbsolutePath(), channelPosterFile.delete());
+            }
+            // 删除数据库记录
+            this.videoFileMapper.deleteById(vf.getId());
+        }
     }
 
     private void validateOperationVideoExists(Integer id) {
